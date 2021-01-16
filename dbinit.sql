857a6e3d7a840b876c97ae97ee883e8cc6e58680
Stuart Fong <72262579+stuartfong1@users.noreply.github.com>
2021-01-16 02:34:19 -0500

    Add files via upload

---
+++ dbinit.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS navicourse CASCADE;
+
+CREATE DATABASE navicourse;
+
+CREATE TABLE navicourse.users (
+    user_id UUID PRIMARY KEY,
+    course_id INT8,
+    rating FLOAT8
+)
+
+IMPORT TABLE navicourse.courses (
+    course_id UUID PRIMARY KEY,
+    title STRING,
+    description STRING,
+    rating INT8,
+    link STRING,
+    source STRING
+) 
+CSV DATA ('<Insert Link Here>');
