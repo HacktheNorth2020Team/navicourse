40d37754fa29bc8fb4ec1a52d8156d8fdcd59a18
Sharif Natheir <sharifnatheir@gmail.com>
2021-01-15 23:53:17 -0500

    base html

--- navicourse.py
+++ navicourse.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return("Hello World!")
+    return render_template("index.html")
 
 
 
+@app.route("/about")
+def about():
+    return render_template("index.html")
+
+
+
+@app.route("/login")
+def login():
+    return render_template("index.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("index.html")
